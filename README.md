1d1084d9bf2a172a1a8557d9ddced45d615c7406
wiltonjunior <wiltonjunior2010@gmail.com>
2024-09-01 15:06:05 -0300

    fix: update componentes

--- README.md
+++ README.md
@@ -1,50 +1,21 @@
-# React + TypeScript + Vite
+# Marvel Super Heros
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Aplicação construida utilizando tenologias como: 
+- Reactjs
+- TypeScript
+- Tailwindui
 
-Currently, two official plugins are available:
+### Getting Started
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Para execultar a aplicação e necessário a configuração das chaves publicas e privadas no site da [Marvel]("https://developer.marvel.com/documentation/authorization")
 
-## Expanding the ESLint configuration
+As configurações devem ser informadas no arquivo .env desse projeto, seguindo o modelo abaixo: 
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+- VITE_PUBLIC_KEY=61w7w918ewa7ewfb25d270d2aw40w98w
+- VITE_PRIVATE_KEY=13ea5wb759e6w2175992a055df0wefa75dd9fcc996c
+- VITE_API_URL=http://gateway.marvel.com/v1
 
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
+Para a rodar o projeto bastar execultar os seguintes comandos:
+  
+- npm install 
+- npm run dev
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,35 +1,69 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router';
 
+import IPagination from '@/models/Pagination';
 import { ICharacters } from '@/models/Characters';
-import { getCharactersById } from '@/services/characters'
+import { getCharactersById, getComicsByCharacters } from '@/services/characters'
 
 import Comics from '@/containers/Comics';
 import Skeleton from '@/components/Skeleton';
+import Button from '@/components/Button';
+import { MoveLeft, MoveRight } from 'lucide-react';
 
 const Detail: React.FC = () => {
 
   const { id } = useParams();
-  const { data: result, isLoading } = useQuery(["getCharactersById", id], async () => {
+  const [page, setPage] = useState<number>(0)
+  const [filters, setFilters] = useState<IPagination>({ offset: 0, limit: 6 })
+
+  const { data: character, isLoading: isLoadingCharacter } = useQuery(["getCharactersById", id], async () => {
     const { data } = await getCharactersById(id as string)
     return data as ICharacters
   }, {
     refetchOnWindowFocus: false,
   });
 
-  const getComponent = (): React.ReactElement => {
-    if (isLoading) {
+  const { data: comics, isLoading: isLoadingComics } = useQuery(["getComicsByCharacters", id, page], async () => {
+    const { data } = await getComicsByCharacters(id as string, filters)
+    return data?.results;
+  }, {
+    refetchOnWindowFocus: false,
+  });
+
+
+  const onUpdatePagination = (page: number) => {
+    const { limit } = filters
+    setFilters({ ...filters, offset: limit * page })
+  }
+
+  const handleNextPage = () => {
+    const value = page + 1;
+    setPage(value)
+    onUpdatePagination(value);
+  }
+
+  const handleBackPage = () => {
+    const value = page - 1;
+    if (value >= 0) {
+      setPage(value)
+      onUpdatePagination(value);
+    }
+  }
+
+  const getPictureComponentWithDescription = (): React.ReactElement => {
+    if (isLoadingCharacter) {
       return <Skeleton className="w-full h-full bg-slate-100" />
     }
     return (
       <div className='h-full border-2 border-black bg-black mb-4'>
         <figure className="relative">
           <div className="transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-            <img className='w-full' src={`${result?.thumbnail.path}.${result?.thumbnail.extension}`} alt="personagem" />
+            <img className='w-full h-full' src={`${character?.thumbnail.path}.${character?.thumbnail.extension}`} alt="personagem" />
           </div>
-          <div className=' py-4 w-full  bottom-0 flex items-center justify-center'>
-            <figcaption className="px-4 text-xl text-white font-bangers">
-              {result?.description || "-"}
+          <div className='w-full flex items-center justify-center'>
+            <figcaption className="p-2 text-xl text-white font-bangers">
+              {character?.description || "-"}
             </figcaption>
           </div>
         </figure>
@@ -37,14 +71,60 @@ const Detail: React.FC = () => {
     )
   }
 
-  return (
-    <div className='grid grid-cols-5 gap-10 my-10'>
-      <div className='col-span-2 h-[max-content]'>
-        <h2 className='font-bangers text-2xl mb-2'>{result?.name}</h2>
-        {getComponent()}
+  const getComicsList = (): React.ReactElement => {
+    if (isLoadingComics) {
+      return <div className='grid grid-cols-2 gap-4 max-sm:grid-cols-1'>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div key={index} className="grid grid-cols-6 gap-4">
+            <div className='col-span-2'>
+              <Skeleton className="w-full h-44 bg-slate-100" />
+            </div>
+            <div className='col-span-4 gap-4'>
+              <Skeleton className="w-52 h-4 mb-3 bg-slate-100" />
+              <Skeleton className="w-40 h-4 mb-3 bg-slate-100" />
+              <Skeleton className="w-20 h-4 mb-3 bg-slate-100" />
+              <Skeleton className="w-48 h-4 mb-3 bg-slate-100" />
+            </div>
+          </div>
+        ))}
       </div>
-      <div className='col-span-3 h-[max-content]'>
-        <Comics />
+    }
+    if (comics && comics.length) {
+      return <div className='grid grid-cols-2 gap-4 max-sm:grid-cols-1'>
+        {comics.map(item => <Comics key={item.id} item={item} />)}
+      </div>
+    }
+    return <div className="w-full h-40 ">
+      <p className="font-bangers text-xl text-black">Não há personagens a serem carregados</p>
+    </div>
+  }
+
+
+  return (
+    <div className='grid grid-cols-5 grid-rows-[1fr, 50px] gap-x-10 my-10 max-sm:flex max-sm:flex-col'>
+      <div className='col-span-2'>
+        <h2 className='font-bangers text-2xl mb-2'>{character?.name}</h2>
+        {getPictureComponentWithDescription()}
+      </div>
+      <div className='row-span-1 col-span-3'>
+        <h2 className='font-bangers text-2xl mb-2'>Quadrinhos</h2>
+        {getComicsList()}
+      </div>
+      <div className='col-span-6 flex mt-4 justify-end items-center gap-4 max-sm:flex-col'>
+        <Button
+          label='Pagina Anterior'
+          iconLeft={<MoveLeft />}
+          onClick={handleBackPage}
+          className='h-8 text-sm p-10'
+          disabled={isLoadingComics || !comics?.length}
+        />
+        <Button
+          label='Proxima pagina'
+          onClick={handleNextPage}
+          iconRight={<MoveRight />}
+          className='h-8 text-sm p-10'
+          disabled={isLoadingComics || !comics?.length}
+        />
       </div>
     </div>
   )
